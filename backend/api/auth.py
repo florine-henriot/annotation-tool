331b86c56530249443ec950ab8baf528b192cf5d
Florine.HENRIOT <fhenriot98@gmail.com>
2025-09-04 22:35:30 +0200

    Fix flavicon

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -15,16 +15,68 @@ router = APIRouter(
     tags = ["auth"]
 )
 
+
 @router.get("/protected")
 def protected_route(current_user: str = Depends(get_current_user)):
+    """
+    Route protégée accessible uniquement aux utilisateurs authentifiés.
+
+    Cette route utilise la dépendant "get_current_user", qui lit et valide le cookie "access_token" (JWT).
+    Si le jeton est valide, l'utilisateur est considéré comme authentifié et son identité est
+    injectée dans la fonction via le paramètre "current_user".
+
+    Etapes : 
+        1. Récupère l'utilisateur courant via "Depends(get_current_user)"
+        2. Si aucun utilisateur n'est trouvé ou si le JWT est invalide : 
+            - Renvoie une erreur 401 "Non autorisé"
+        3. Sinon, retourne un message de bienvenue avec l'identifiant de l'utilisateur.
+
+    Paramètres :
+        current_user (str) : Identifiant de l'utilisateur authentifié (son email),
+        fourni avec current_user.
+
+    Exceptions:
+        HTTPException(401) : Si aucun utilisateur n'est authentifié ou si le 
+            jeton d'accès est invalide/expiré.
+
+    Returns:
+        dict : {message : Bienvenue <utilisateur> ! }
+    """
     if not current_user:
         raise HTTPException(status_code=401, detail="Non autorisé")
     return {"message": f"Bienvenue {current_user} !"}
 
 
-
 @router.post("/login")
 def login(request: LoginRequest, db: Session = Depends(get_db)):
+    """
+    Authentifie un utilisateur et génère un jeton d'accès JWT stocké dans un cookie HTTPOnly.
+
+    Etapes:
+
+    1. Recherche l'utilisateur par email dans la base de données.
+    2. Vérifie si le compte est temporairement vérouillé suite à trop de tentatives échouées.
+    3. Vérifie la validité du mot de passe fourni : 
+        - Si incorrect : incrément le compte d'échecs et vérouille le compte après N tentatives.
+        - Si correct : réinitialise le compteur et retire tout verrouillage.
+    4. Génère un jeton JWT contenant l'email de l'utilisateur.
+    5. Retourne une réponse JSON avec un cookie 'access_token':
+        - httponly=True --> inaccessible côté JavaScript
+        - secure=False en dev, secure=True recommandé en production
+        - samesite="strict" pour limiter les attaques CRSF
+        - max_age=3600 (1h)
+
+    Paramètres : 
+        request (LoginRequest) : Object contenant l'email et le mot de passe de l'utilisateur.
+        db (Session) : Session SQLAlchemy injectée via Depends(get_db)
+
+    Exceptions : 
+        HTTPException(401) : Identifiants incorrects (email inexistant ou mauvais mot de passe)
+        HTTPException(403) : Compte vérouillé suite à trop de tentatives échouées.
+
+    Returns:
+        JSONResponse : {sucess:True} avec un cookie HTTPOnly "access_token" valide pendant 1h.
+    """
     user = get_user_by_email(db, request.email)
     if not user :
         raise HTTPException(status_code = 401, detail = "Identifiants incorrects.")
@@ -59,10 +111,11 @@ def login(request: LoginRequest, db: Session = Depends(get_db)):
         httponly=True,
         secure=False, # True en prod avec HTTPS
         samesite="strict",
-        max_age = 3600 # 30 minutes
+        max_age = 3600 # 1h
     )
 
     return response
+
 
 @router.post("/logout")
 def logout(response: Response):
