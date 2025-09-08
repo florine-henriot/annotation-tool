34f4dd4b4a63f5c66cb725fb4ff74e329c42a42e
Florine.HENRIOT <fhenriot98@gmail.com>
2025-09-08 22:43:29 +0200

    Feat deconnect + account button topbar

--- frontend/labelia-frontend/src/components/common/Topbar.js
+++ frontend/labelia-frontend/src/components/common/Topbar.js
@@ -1,7 +1,34 @@
+import React from 'react';
+import { useNavigate, NavLink } from 'react-router-dom';
+import axiosClient from "../api/axiosClient";
+import { LogOut, User } from 'lucide-react'
+import Popup from './Popup';
 import "../../App.css";
 import "./Topbar.css";
 
 export default function TopBar( { icon: Icon, pageTitle }) {
+    const navigate = useNavigate();
+    const [showTooltip, setShowTooltip] = React.useState(false);
+    const [showPopup, setShowPopup] = React.useState(false);
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+        navigate('/login')
+    }
+
+    const handleLogout = async () => {
+        try {
+            await axiosClient.post("/auth/logout", {}, {withCredentials: true});
+            setShowPopup(true);
+            setTimeout(() => {
+                setShowPopup(false);
+                navigate("/login")
+            }, 2000);
+        } catch (err) {
+            console.error("Erreur lors de la déconnexion :", err);
+        }
+    };
+
     return (
         <div className="topbar-common">
             <img src='./avatar/logo_labelia.png' alt='Logo' className='logo' />
@@ -13,6 +40,33 @@ export default function TopBar( { icon: Icon, pageTitle }) {
                     {pageTitle}
                 </div>
             </span>
+
+            <div className="right-element">
+
+                <span>
+                    <NavLink to="/account" className="account-button">
+                        <User size={18} style={{marginRight: "5px"}} />
+                        Mon compte
+                    </NavLink>
+                </span>
+
+                <span>
+                    <button type="submit" 
+                        className="deconnect-button" 
+                        onMouseEnter={() => setShowTooltip(true)} 
+                        onMouseLeave={() => setShowTooltip(false)}
+                        onClick={handleLogout}>
+                        <LogOut size={18} />
+                    </button>
+                    {showTooltip && <span className='tooltip'>Déconnexion</span>}
+                </span>
+            </div>
+
+            {showPopup && (
+                <Popup onClose={handleClosePopup}>
+                    Vous allez être déconnecté.
+                </Popup>
+            )}
         </div>
     )
 }
