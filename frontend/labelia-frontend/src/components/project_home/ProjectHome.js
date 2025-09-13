0432199f545b46ba84296b99945ca2ed5a58b421
Florine.HENRIOT <fhenriot98@gmail.com>
2025-09-13 11:51:04 +0200

    Begin project home page

--- frontend/labelia-frontend/src/components/project_home/ProjectHome.js
+++ frontend/labelia-frontend/src/components/project_home/ProjectHome.js
@@ -1,5 +1,37 @@
+import "./ProjectHome.css";
+
 export default function ProjectHome() {
     return (
-        <span>Test</span>
+        <div className="container-project-home">
+            <div className="cell">
+                <h2 className="project-home-subtitle">
+                    Répartition des catégories
+                </h2>
+            </div>
+            
+            <div className="cell">
+                <h2 className="project-home-subtitle">
+                    Tracker des annotations
+                </h2>
+            </div>
+
+            <div className="cell">
+                <h2 className="project-home-subtitle">
+                    Date d'échéance
+                </h2>
+            </div>
+
+            <div className="cell">
+                <h2 className="project-home-subtitle">
+                    Nombre moyen d'annotations par jour
+                </h2>
+            </div>
+
+            <div className="cell">5</div>
+            <div className="cell">6</div>
+            <div className="cell">7</div>
+            <div className="cell">8</div>
+            <div className="cell">9</div>
+        </div>
     )
 }
